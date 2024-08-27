6a5a2c4412911f8e338659d9322b7c6b058c4a42
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-27 11:45:46 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 523ms, Memory: 28MB, Status: Passed - CodeTree

--- 240827/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240827/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -1,7 +1,6 @@
 dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 n, m = map(int, input().split())
-
 board = [list(map(int, input().split())) for _ in range(n)]
 
 result = 0
@@ -20,32 +19,34 @@ def block1(x, y, acc, depth):
         nx, ny = x + dx, y + dy
 
         if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-            visited[x][y] = True
+            visited[nx][ny] = True
             block1(nx, ny, acc + board[nx][ny], depth + 1)
-            visited[x][y] = False
+            visited[nx][ny] = False
 
 
 # ㅗ 블록
 def block2(x, y):
     global result
-    path = [board[x][y]]
-    for dx, dy in dir:
-        nx, ny = x + dx, y + dy
 
-        if 0 <= nx < n and 0 <= ny < m:
-            path.append(board[nx][ny])
-
-    if len(path) == 4:
-        result = max(result, sum(path))
-    elif len(path) == 5:
-        path.sort()
-        result = max(result, sum(path[1:]))
+    if x >= 1 and 1 <= y < m - 1:
+        # ㅗ 모양
+        result = max(result, board[x][y] + board[x-1][y] + board[x][y-1] + board[x][y+1])
+    if x < n - 1 and 1 <= y < m - 1:
+        # ㅜ 모양
+        result = max(result, board[x][y] + board[x+1][y] + board[x][y-1] + board[x][y+1])
+    if 1 <= x < n - 1 and y >= 1:
+        # ㅓ 모양
+        result = max(result, board[x][y] + board[x-1][y] + board[x+1][y] + board[x][y-1])
+    if 1 <= x < n - 1 and y < m - 1:
+        # ㅏ 모양
+        result = max(result, board[x][y] + board[x-1][y] + board[x+1][y] + board[x][y+1])
 
 
 for i in range(n):
-    for j in range(m):
+    for j in range(m):  # 열의 개수는 m
         visited[i][j] = True
         block1(i, j, board[i][j], 1)
         visited[i][j] = False
         block2(i, j)
+
 print(result)
