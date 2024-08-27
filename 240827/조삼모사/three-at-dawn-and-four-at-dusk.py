1e6bc505dc15b219a4fbaf3b07a0b1becf97409f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-27 12:52:00 +0900

    Title: 조삼모사, Time: 5018ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 240827/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -0,0 +1,33 @@
+import math
+
+
+def dfs(path, depth, start):
+    global result
+    if depth == n // 2:
+        morning = 0
+        night = 0
+
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                else:
+                    if i in path and j in path:
+                        morning += board[i][j]
+                    elif i not in path and j not in path:
+                        night += board[i][j]
+
+        result = min(result, abs(morning - night))
+
+        return
+
+    for i in range(start, n):
+        dfs(path | {i}, depth + 1, i + 1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    result = math.inf
+    dfs(set(), 0, 0)
+    print(result)
