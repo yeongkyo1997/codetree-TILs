6e446facdf05de0d5a3e5b42df3fee66b2e90c20
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-27 13:00:57 +0900

    Title: 조삼모사, Time: 766ms, Memory: 29MB, Status: Passed - CodeTree

--- 240827/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 240827/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -3,25 +3,21 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 
-
 def dfs(path, depth, start):
     global result
     if depth == n // 2:
         morning = 0
         night = 0
 
-        for i in range(n):
-            for j in range(n):
-                if i == j:
-                    continue
-                else:
-                    if i in path and j in path:
-                        morning += board[i][j]
-                    elif i not in path and j not in path:
-                        night += board[i][j]
+        morning_indices = list(path)
+        night_indices = [i for i in range(n) if i not in path]
+
+        for i in range(n // 2):
+            for j in range(i + 1, n // 2):
+                morning += board[morning_indices[i]][morning_indices[j]] + board[morning_indices[j]][morning_indices[i]]
+                night += board[night_indices[i]][night_indices[j]] + board[night_indices[j]][night_indices[i]]
 
         result = min(result, abs(morning - night))
-
         return
 
     for i in range(start, n):
@@ -30,7 +26,6 @@ def dfs(path, depth, start):
         path.add(i)
         dfs(path, depth + 1, i + 1)
         path.discard(i)
-
 
 if __name__ == '__main__':
     n = int(input())
