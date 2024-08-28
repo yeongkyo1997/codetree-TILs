df0ec23880cadc52050e14c0b4ec92945510df05
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-28 14:27:20 +0900

    Title: 격자 숫자 놀이, Time: 332ms, Memory: 27MB, Status: Passed - CodeTree

--- 240828/격자 숫자 놀이/matrix-number-play.py
+++ 240828/격자 숫자 놀이/matrix-number-play.py
@@ -3,22 +3,36 @@ import collections
 
 def insert_num(arr):
     cnt_num = dict(collections.Counter(arr))
+    arr = [x for x in arr if x != 0]  # 0 제거
     arr.sort(key=lambda x: (cnt_num[x], x))
 
     new_arr = []
-
     visited = set()
     for a in arr:
         if a in visited:
             continue
-        new_arr.append(a)
-        new_arr.append(cnt_num[a])
+        new_arr.extend([a, cnt_num[a]])
         visited.add(a)
 
-    if len(new_arr) > 100:
-        new_arr = new_arr[:100]
-
     return new_arr
+
+
+def r_operation(board):
+    new_board = []
+    for row in board:
+        new_row = insert_num(row)
+        new_row = new_row[:100]  # 100개로 제한
+        new_board.append(new_row)
+    max_len = max(len(row) for row in new_board)
+    for row in new_board:
+        row.extend([0] * (max_len - len(row)))
+    return new_board
+
+
+def c_operation(board):
+    board = list(map(list, zip(*board)))  # 전치
+    board = r_operation(board)
+    return list(map(list, zip(*board)))  # 다시 전치
 
 
 if __name__ == '__main__':
@@ -27,30 +41,14 @@ if __name__ == '__main__':
 
     result = -1
     for time in range(101):
-        if r - 1 < len(board) and c - 1 < len(board[0]) and board[r - 1][c - 1] == k:
-            result = time
-            break
+        if r - 1 < len(board) and c - 1 < len(board[0]):
+            if board[r - 1][c - 1] == k:
+                result = time
+                break
 
         if len(board) >= len(board[0]):
-            new_board = []
-            max_len = 0
-            for row in board:
-                new_row = insert_num(row)
-                new_board.append(new_row)
-                max_len = max(max_len, len(new_row))
-            for row in new_board:
-                row.extend([0] * (max_len - len(row)))
-            board = new_board
+            board = r_operation(board)
         else:
-            board = list(map(list, zip(*board)))
-            new_board = []
-            max_len = 0
-            for col in board:
-                new_col = insert_num(col)
-                new_board.append(new_col)
-                max_len = max(max_len, len(new_col))
-            for col in new_board:
-                col.extend([0] * (max_len - len(col)))
-            board = list(map(list, zip(*new_board)))
+            board = c_operation(board)
 
     print(result)
