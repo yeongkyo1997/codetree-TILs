f20b3f15b603f2586064bef3f1f05e7c6b756b85
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-02 13:20:33 +0900

    Title: 2048 게임, Time: 364ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240902/2048 게임/2048-game.py
@@ -0,0 +1,139 @@
+import copy
+
+
+def right(board):
+    for row in range(n):
+        merged = [False] * n
+        for pivot in range(n - 1, 0, -1):
+            if board[row][pivot] == 0:
+                continue
+            for i in range(pivot - 1, -1, -1):
+                if board[row][i] == 0:
+                    continue
+                if board[row][pivot] == board[row][i] and not merged[pivot]:
+                    board[row][pivot] *= 2
+                    board[row][i] = 0
+                    merged[pivot] = True
+                elif board[row][pivot] == 0:
+                    board[row][pivot] = board[row][i]
+                    board[row][i] = 0
+                break
+        # 빈 공간을 채우기 위해 다시 오른쪽으로 밀기
+        for i in range(n - 1, 0, -1):
+            if board[row][i] == 0:
+                for j in range(i - 1, -1, -1):
+                    if board[row][j] != 0:
+                        board[row][i] = board[row][j]
+                        board[row][j] = 0
+                        break
+
+
+# 왼쪽, 위, 아래 함수도 비슷하게 수정
+
+def left(board):
+    for row in range(n):
+        merged = [False] * n
+        for pivot in range(n - 1):
+            if board[row][pivot] == 0:
+                continue
+            for i in range(pivot + 1, n):
+                if board[row][i] == 0:
+                    continue
+                if board[row][pivot] == board[row][i] and not merged[pivot]:
+                    board[row][pivot] *= 2
+                    board[row][i] = 0
+                    merged[pivot] = True
+                elif board[row][pivot] == 0:
+                    board[row][pivot] = board[row][i]
+                    board[row][i] = 0
+                break
+        for i in range(n - 1):
+            if board[row][i] == 0:
+                for j in range(i + 1, n):
+                    if board[row][j] != 0:
+                        board[row][i] = board[row][j]
+                        board[row][j] = 0
+                        break
+
+
+def up(board):
+    for col in range(n):
+        merged = [False] * n
+        for pivot in range(n - 1):
+            if board[pivot][col] == 0:
+                continue
+            for i in range(pivot + 1, n):
+                if board[i][col] == 0:
+                    continue
+                if board[pivot][col] == board[i][col] and not merged[pivot]:
+                    board[pivot][col] *= 2
+                    board[i][col] = 0
+                    merged[pivot] = True
+                elif board[pivot][col] == 0:
+                    board[pivot][col] = board[i][col]
+                    board[i][col] = 0
+                break
+        for i in range(n - 1):
+            if board[i][col] == 0:
+                for j in range(i + 1, n):
+                    if board[j][col] != 0:
+                        board[i][col] = board[j][col]
+                        board[j][col] = 0
+                        break
+
+
+def down(board):
+    for col in range(n):
+        merged = [False] * n
+        for pivot in range(n - 1, 0, -1):
+            if board[pivot][col] == 0:
+                continue
+            for i in range(pivot - 1, -1, -1):
+                if board[i][col] == 0:
+                    continue
+                if board[pivot][col] == board[i][col] and not merged[pivot]:
+                    board[pivot][col] *= 2
+                    board[i][col] = 0
+                    merged[pivot] = True
+                elif board[pivot][col] == 0:
+                    board[pivot][col] = board[i][col]
+                    board[i][col] = 0
+                break
+        for i in range(n - 1, 0, -1):
+            if board[i][col] == 0:
+                for j in range(i - 1, -1, -1):
+                    if board[j][col] != 0:
+                        board[i][col] = board[j][col]
+                        board[j][col] = 0
+                        break
+
+
+def dfs(board, depth):
+    global result
+
+    if depth == 5:
+        for i in range(n):
+            for j in range(n):
+                result = max(result, board[i][j])
+        return
+
+    tmp = [b[:] for b in board]
+    left(tmp)
+    dfs(tmp, depth + 1)
+    tmp = [b[:] for b in board]
+    right(tmp)
+    dfs(tmp, depth + 1)
+    tmp = [b[:] for b in board]
+    up(tmp)
+    dfs(tmp, depth + 1)
+    tmp = [b[:] for b in board]
+    down(tmp)
+    dfs(tmp, depth + 1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    result = 0
+    dfs(board, 0)
+    print(result)
