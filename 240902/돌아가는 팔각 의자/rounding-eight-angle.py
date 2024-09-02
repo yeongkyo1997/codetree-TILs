7422a001b814df61f6fc8599090fbe779a1aa8c7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-02 13:23:40 +0900

    Title: 돌아가는 팔각 의자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240902/돌아가는 팔각 의자/rounding-eight-angle.py
@@ -0,0 +1,42 @@
+import collections
+
+
+def rotate(n, d):
+    tmp = [b[:] for b in board]
+    board[n].rotate(d)
+
+    nn, nd = n, d
+    # 오른쪽
+    while nn + 1 <= 4:
+        if tmp[nn][2] == tmp[nn + 1][-2]:
+            break
+        else:
+            nd *= -1
+            board[nn + 1].rotate(nd)
+        nn += 1
+
+    nn, nd = n, d
+    # 왼쪽
+    while nn - 1 >= 1:
+        if tmp[nn][-2] == tmp[nn - 1][2]:
+            break
+        else:
+            nd *= -1
+            board[nn - 1].rotate(nd)
+        nn -= 1
+
+
+if __name__ == '__main__':
+    board = [[]]
+    for _ in range(4):
+        board.append(collections.deque(list(map(int, input()))))
+
+    k = int(input())
+    for _ in range(k):
+        rotate(*map(int, input().split()))
+
+    result = 0
+    for i, b in enumerate(board[1:]):
+        result += b[0] << i
+
+    print(result)
