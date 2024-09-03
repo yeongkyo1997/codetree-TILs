24d455dde14a91073f7aa3cf243a3c689d90dc6d
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-03 16:28:43 +0900

    Title: 코드트리 빵, Time: 5095ms, Memory: 139MB, Status: Time Limit Exceed - CodeTree

--- 240903/코드트리 빵/codetree-mon-bread.py
+++ 240903/코드트리 빵/codetree-mon-bread.py
@@ -1,4 +1,5 @@
 import heapq
+import math
 
 
 # 사람들 이동시키기
@@ -7,9 +8,7 @@ def bfs():
     heapq.heapify(people)
 
     while people:
-        p = heapq.heappop(people)
-        t, _, x, y = p[0]
-        idx = p[1]
+        t, _, x, y, idx = heapq.heappop(people)
         if m == len(finished_people):
             break
         if idx in finished_people:
@@ -18,7 +17,7 @@ def bfs():
         for i, (dx, dy) in enumerate(dir):
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < n and 0 <= ny < n and times[nx][ny] <= t + 1:
+            if 0 <= nx < n and 0 <= ny < n and times[nx][ny] >= t + 1:
                 if board[nx][ny] == 2:
                     if t + 1 == times[nx][ny]:
                         continue
@@ -26,7 +25,7 @@ def bfs():
                     finished_people.add(idx)
                     times[nx][ny] = t + 1
                     result = max(result, t + 1)
-                heapq.heappush(people, ((t + 1, i, nx, ny), idx))
+                heapq.heappush(people, (t + 1, i, nx, ny, idx))
 
 
 # 편의점에서 가장 가까운 베이스 캠프 구하기
@@ -48,7 +47,7 @@ def get_shortest_basecamp(x, y):
                 if board[nx][ny] == 1:
                     board[nx][ny] = 2
                     return nx, ny
-                heapq.heappush(heap, (depth + 1, nx, ny))
+                heapq.heappush(heap, ((depth + 1, nx, ny)))
 
 
 if __name__ == '__main__':
@@ -61,7 +60,7 @@ if __name__ == '__main__':
     for _ in range(m):
         x, y = map(lambda x: int(x) - 1, input().split())
         store.append((x, y))
-    times = [[0] * n for _ in range(n)]
+    times = [[math.inf] * n for _ in range(n)]
     finished_people = set()
     result = 0
     # 1분씩 증가 시켜서 프로세스 진행
@@ -75,7 +74,7 @@ if __name__ == '__main__':
             # 베이스 캠프에 들어간 시점 이후 시간은 방문이 안됨 같으면 들어갈수있음
             times[sx][sy] = t
             # 사람 좌표에 번호를 추가하기, 시작시간을 넣음
-            people.append(((t, 0, sx, sy), t))
+            people.append((t, 0, sx, sy, t))
 
     bfs()
     print(result)
