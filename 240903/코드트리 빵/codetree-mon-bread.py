5dc595c35bdb333933b9b61325eead69b6b42c51
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-03 15:53:25 +0900

    Title: 코드트리 빵, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240903/코드트리 빵/codetree-mon-bread.py
+++ 240903/코드트리 빵/codetree-mon-bread.py
@@ -7,21 +7,26 @@ def bfs():
     heapq.heapify(people)
 
     while people:
-        t, x, y, idx = heapq.heappop(people)
+        p = heapq.heappop(people)
+        t, _, x, y = p[0]
+        idx = p[1]
         if m == len(finished_people):
             break
         if idx in finished_people:
             continue
 
-        for dx, dy in dir:
+        for i, (dx, dy) in enumerate(dir):
             nx, ny = x + dx, y + dy
 
             if 0 <= nx < n and 0 <= ny < n and times[nx][ny] <= t + 1:
+                if board[nx][ny] == 2:
+                    if t + 1 == times[nx][ny]:
+                        continue
                 if (nx, ny) == store[idx]:
                     finished_people.add(idx)
                     times[nx][ny] = t + 1
                     result = max(result, t + 1)
-                heapq.heappush(people, (t + 1, nx, ny, idx))
+                heapq.heappush(people, ((t + 1, i, nx, ny), idx))
 
 
 # 편의점에서 가장 가까운 베이스 캠프 구하기
@@ -70,7 +75,7 @@ if __name__ == '__main__':
             # 베이스 캠프에 들어간 시점 이후 시간은 방문이 안됨 같으면 들어갈수있음
             times[sx][sy] = t
             # 사람 좌표에 번호를 추가하기, 시작시간을 넣음
-            people.append((t, sx, sy, t))
+            people.append(((t, 0, sx, sy), t))
 
     bfs()
     print(result)
