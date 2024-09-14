0047e0201075b570f9300a9c4d20bccc42f3b08a
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-14 17:46:15 +0900

    Title: 나무박멸, Time: 161ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240914/나무박멸/tree-kill-all.py
+++ 240914/나무박멸/tree-kill-all.py
@@ -1,5 +1,3 @@
-import collections
-import math
 import sys
 
 # sys.stdin = open('나무박멸', 'r')
@@ -7,107 +5,103 @@ import sys
 
 # 나무 성장
 def grow():
-    dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    for x in range(N):
-        for y in range(N):
-            if board[x][y] <= 0:
-                continue
-            cnt = 0
-            for dx, dy in dir:
-                nx, ny = x + dx, y + dy
+    def grow(x, y):
+        ret = 0
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = x + dx, y + dy
 
-                if 0 <= nx < N and 0 <= ny < N and board[nx][ny] > 0 and death[nx, ny] == 0:
-                    cnt += 1
-            board[x][y] += cnt
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] > 0:
+                ret += 1
+        return ret
+
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] > 0:
+                board[i][j] += grow(i, j)
+    return board
 
 
 # 나무 번식
 def spread():
-    global board
     dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    tmp = [b[:] for b in board]
-    for x in range(N):
-        for y in range(N):
-            if board[x][y] <= 0:
+    tmp = [row[:] for row in board]
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] <= 0:
                 continue
-            cnt = 0
             candi = []
             for dx, dy in dir:
-                nx, ny = x + dx, y + dy
-
-                if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0 and death[nx, ny] == 0:
-                    cnt += 1
+                nx, ny = i + dx, j + dy
+                if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0 and death[nx][ny] == 0:
                     candi.append((nx, ny))
             for cx, cy in candi:
-                tmp[cx][cy] += board[x][y] // cnt
+                tmp[cx][cy] += board[i][j] // len(candi)
 
-    board = tmp
+    return tmp
 
 
 # 제초제 뿌리기
 def remove():
-    global board, death, result
-
-    dir = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
-
-    def do(x, y):
+    # 제거되는 나무 수
+    def calc(x, y):
         ret = board[x][y]
-        for dx, dy in dir:
-            for i in range(1, K + 1):
-                nx, ny = x + dx * i, y + dy * i
-
-                if 0 <= nx < N and 0 <= ny < N:
-                    if board[nx][ny] <= 0:
-                        break
+        for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            for size in range(1, K + 1):
+                nx, ny = x + dx * size, y + dy * size
+                if 0 <= nx < N and 0 <= ny < N and board[nx][ny] > 0:
                     ret += board[nx][ny]
+                else:
+                    break
         return ret
 
-    max_val = -math.inf
-    rx, ry = -1, -1
-    for x in range(N):
-        for y in range(N):
-            if board[x][y] > 0:
-                val = do(x, y)
-                if max_val < val:
-                    max_val = val
-                    rx, ry = x, y
-    if (rx, ry) == (-1, -1):
-        return
-    result += board[rx][ry]
-    board[rx][ry] = 0
-    death[rx, ry] = C + 1
-    for dx, dy in dir:
-        for i in range(1, K + 1):
-            nx, ny = rx + dx * i, ry + dy * i
-            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] != -1:
-                if board[nx][ny] == 0:
-                    death[nx, ny] = C + 1
-                    break
-                death[nx, ny] = C + 1
-                result += board[nx][ny]
+    candi = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] > 0:
+                candi.append((-calc(i, j), i, j, (i, j)))
+    if not candi:
+        return -1
+    candi.sort()
+    x, y = candi[0][-1]
+    ret = board[x][y]
+    board[x][y] = 0
+    death[x][y] = C + 1
+    for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+        for size in range(1, K + 1):
+            nx, ny = x + dx * size, y + dy * size
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] > 0:
+                ret += board[nx][ny]
                 board[nx][ny] = 0
+                death[nx][ny] = C + 1
             else:
                 break
+
+    return ret
 
 
 # 제초제 줄이기
 def reduce():
     for i in range(N):
         for j in range(N):
-            if death[i, j] != 0:
-                death[i, j] -= 1
+            death[i][j] = max(0, death[i][j])
 
 
 if __name__ == '__main__':
     N, M, K, C = map(int, input().split())
+
     board = [list(map(int, input().split())) for _ in range(N)]
-    death = collections.defaultdict(int)
+    # 제초제
+    death = [[0] * N for _ in range(N)]
 
     result = 0
-    for i in range(M):
+    # 로직 시작
+    for _ in range(M):
+        board = grow()
+        board = spread()
+        val = remove()
+        if val < 0:
+            break
+        result += val
         reduce()
-        grow()
-        spread()
-        remove()
 
     print(result)
