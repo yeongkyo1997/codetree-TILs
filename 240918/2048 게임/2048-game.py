42c37fe0c18cffb0be8adc1586713055768edf57
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-18 22:52:30 +0900

    Title: 2048 게임, Time: 312ms, Memory: 28MB, Status: Passed - CodeTree

--- 240918/2048 게임/2048-game.py
+++ 240918/2048 게임/2048-game.py
@@ -6,99 +6,145 @@ import sys
 # 상
 def up(board):
     for col in range(N):
-        merged = False
-        for pivot in range(N):
+        pivot = 0
+        merged = set()
+        while True:
+            if pivot == N:
+                break
             for row in range(pivot + 1, N):
+                # 현재 값이 0이라면 무시
                 if board[row][col] == 0:
                     continue
 
-                # 기준과 움직이려는 값이 같고 아직 합친적이 없다면
-                if board[row][col] == board[pivot][col] and not merged:
-                    merged = True
-                    board[pivot][col] *= 2
+                # pivot에 있는 값이 0이라면 바로 이동
+                if board[pivot][col] == 0:
+                    board[pivot][col] = board[row][col]
                     board[row][col] = 0
-                    break
                 else:
-                    # 기준이 0이라면 움직이려는 값을 기준으로 이동
-                    if board[pivot][col] == 0:
-                        board[pivot][col], board[row][col] = board[row][col], board[pivot][col]
-                    # 기준이 0이 아니라면 움직이려는 값 다음 칸으로 이동
-                    else:
-                        board[pivot + 1][col], board[row][col] = board[row][col], board[pivot + 1][col]
+                    # pivot과 현재 값이 같다면
+                    if board[pivot][col] == board[row][col] and pivot not in merged:
+                        board[row][col] = 0
+                        board[pivot][col] *= 2
+                        merged.add(pivot)
+                        pivot += 1
                         break
+                    # 다르다면 앞 칸에 배치
+                    else:
+                        if board[pivot + 1][col] == 0:
+                            board[pivot + 1][col] = board[row][col]
+                            board[row][col] = 0
+                        pivot += 1
+                        break
+            else:
+                break
 
 
 # 하
 def down(board):
     for col in range(N):
-        merged = False
-        for pivot in range(N - 1, -1, -1):
+        pivot = N - 1
+        merged = set()
+        while True:
+            if pivot == 0:
+                break
             for row in range(pivot - 1, -1, -1):
+                # 현재 값이 0이라면 무시
                 if board[row][col] == 0:
                     continue
 
-                # 기준과 움직이려는 값이 같고 아직 합친적이 없다면
-                if board[row][col] == board[pivot][col] and not merged:
-                    merged = True
-                    board[pivot][col] *= 2
+                # pivot에 있는 값이 0이라면 바로 이동
+                if board[pivot][col] == 0:
+                    board[pivot][col] = board[row][col]
                     board[row][col] = 0
-                    break
                 else:
-                    # 기준이 0이라면 움직이려는 값을 기준으로 이동
-                    if board[pivot][col] == 0:
-                        board[pivot][col], board[row][col] = board[row][col], board[pivot][col]
-                    # 기준이 0이 아니라면 움직이려는 값 다음 칸으로 이동
-                    else:
-                        board[pivot - 1][col], board[row][col] = board[row][col], board[pivot - 1][col]
+                    # pivot과 현재 값이 같다면
+                    if board[pivot][col] == board[row][col] and pivot not in merged:
+                        board[row][col] = 0
+                        board[pivot][col] *= 2
+                        merged.add(pivot)
+                        pivot -= 1
                         break
+                    # 다르다면 앞 칸에 배치
+                    else:
+                        if board[pivot - 1][col] == 0:
+                            board[pivot - 1][col] = board[row][col]
+                            board[row][col] = 0
+                        pivot -= 1
+                        break
+            else:
+                break
 
 
 # 좌
 def left(board):
     for row in range(N):
-        merged = False
-
-        for pivot in range(N):
+        pivot = 0
+        merged = set()
+        while True:
+            if pivot == N:
+                break
             for col in range(pivot + 1, N):
+                # 현재 값이 0이라면 무시
                 if board[row][col] == 0:
                     continue
-                # 기준과 움직이려는 값이 같고 아직 합친적이 없다면
-                if board[row][col] == board[row][pivot] and not merged:
-                    board[row][pivot] *= 2
+
+                # pivot에 있는 값이 0이라면 바로 이동
+                if board[row][pivot] == 0:
+                    board[row][pivot] = board[row][col]
                     board[row][col] = 0
-                    merged = True
-                    break
                 else:
-                    # 기준이 0이라면 움직이려는 값을 기준으로 이동
-                    if board[row][pivot] == 0:
-                        board[row][pivot], board[row][col] = board[row][col], board[row][pivot]
-                    # 기준이 0이 아니라면 움직이려는 값 다음 칸으로 이동
-                    else:
-                        board[row][pivot + 1], board[row][col] = board[row][col], board[row][pivot + 1]
+                    # pivot과 현재 값이 같다면
+                    if board[row][pivot] == board[row][col] and pivot not in merged:
+                        board[row][col] = 0
+                        board[row][pivot] *= 2
+                        merged.add(pivot)
+                        pivot += 1
                         break
+                    # 다르다면 앞 칸에 배치
+                    else:
+                        if board[row][pivot + 1] == 0:
+                            board[row][pivot + 1] = board[row][col]
+                            board[row][col] = 0
+                        pivot += 1
+                        break
+            else:
+                break
 
 
 # 우
 def right(board):
     for row in range(N):
-        merged = False
-        for pivot in range(N - 1, -1, -1):
+        pivot = N - 1
+        merged = set()
+        while True:
+            if pivot == 0:
+                break
             for col in range(pivot - 1, -1, -1):
+                # 현재 값이 0이라면 무시
                 if board[row][col] == 0:
                     continue
-                # 기준과 움직이려는 값이 같고 아직 합친적이 없다면
-                if board[row][col] == board[row][pivot] and not merged:
-                    board[row][pivot] *= 2
+
+                # pivot에 있는 값이 0이라면 바로 이동
+                if board[row][pivot] == 0:
+                    board[row][pivot] = board[row][col]
                     board[row][col] = 0
-                    merged = True
                 else:
-                    # 기준이 0이라면 움직이려는 값을 기준으로 이동
-                    if board[row][pivot] == 0:
-                        board[row][pivot], board[row][col] = board[row][col], board[row][pivot]
-                    # 기준이 0이 아니라면 움직이려는 값 다음 칸으로 이동
-                    else:
-                        board[row][pivot - 1], board[row][col] = board[row][col], board[row][pivot - 1]
+                    # pivot과 현재 값이 같다면
+                    if board[row][pivot] == board[row][col] and pivot not in merged:
+                        board[row][col] = 0
+                        board[row][pivot] *= 2
+                        merged.add(pivot)
+                        pivot -= 1
                         break
+                    # 다르다면 앞 칸에 배치
+                    else:
+                        if board[row][pivot - 1] == 0:
+                            board[row][pivot - 1] = board[row][col]
+                            board[row][col] = 0
+                        pivot -= 1
+                        break
+            else:
+                break
 
 
 # 방향 dfs
