b242d80bad43a790a0f992c22d519a5794cb081f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-18 13:06:05 +0900

    Title: 자율주행 자동차, Time: 200ms, Memory: 25MB, Status: Passed - CodeTree

--- 240918/자율주행 자동차/autonomous-driving.py
+++ 240918/자율주행 자동차/autonomous-driving.py
@@ -9,29 +9,30 @@ def move(sx, sy, sd):
     ret = 0
 
     while True:
-        # 다음 방향 찾기
-        for d in range(len(dir)):
-            nd = (sd + d) % 4
+        # 현재 위치 방문 여부 확인
+        if not visited[sx][sy]:
+            visited[sx][sy] = True
+            ret += 1
+
+        # 다음 위치 찾기
+        for d in range(1, len(dir) + 1):
+            nd = (sd - d) % 4
             dx, dy = dir[nd]
             nx, ny = sx + dx, sy + dy
 
-            # 다음 칸으로 갈 수 있는 경우
+            # 다음 위치를 찾았다면
             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and board[nx][ny] == 0:
-                visited[nx][ny] = True
-                ret += 1
                 sx, sy, sd = nx, ny, nd
                 break
         else:
-            # 다음 칸으로 갈 수 없는 경우
+            # 찾지 못 했다면
             dx, dy = dir[sd]
             nx, ny = sx - dx, sy - dy
-
             # 후진 가능
             if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
                 sx, sy = nx, ny
             else:
                 break
-
     return ret
 
 
