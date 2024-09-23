6c6f19d4ead0b7fda5b34d7c5d5d4150b144eb51
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-23 12:27:03 +0900

    Title: 바이러스 백신, Time: 159ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240923/바이러스 백신/vaccine-for-virus.py
@@ -0,0 +1,49 @@
+import collections
+import math
+import sys
+
+# sys.stdin = open('바이러스 백신', 'r')
+
+
+def bfs(path):
+    dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    ret = 0
+    visited = set()
+    q = collections.deque(map(lambda x: (x[0], x[1], 0), path))
+
+    while q:
+        x, y, depth = q.popleft()
+        ret = max(depth, ret)
+
+        for dx, dy in dir:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0 and (nx, ny) not in visited:
+                visited.add((nx, ny))
+                q.append((nx, ny, depth + 1))
+    return ret if len(visited) == zero_cnt else math.inf
+
+
+# 병원 고르기
+def dfs(path, depth, start):
+    if depth == M:
+        return bfs(path)
+    ret = math.inf
+    for i in range(start, len(hospitals)):
+        ret = min(ret, dfs(path + [hospitals[i]], depth + 1, i + 1))
+    return ret
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    hospitals = []
+    zero_cnt = 0
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 2:
+                hospitals.append((i, j))
+            elif board[i][j] == 0:
+                zero_cnt += 1
+    result = dfs([], 0, 0)
+    print(result if result != math.inf else -1)
