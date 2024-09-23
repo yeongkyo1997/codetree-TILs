93d83862ed600769877ce547068e504d28a946f7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-23 12:09:32 +0900

    Title: 정육면체 한번 더 굴리기, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 240923/정육면체 한번 더 굴리기/cube-rounding-again.py
+++ 240923/정육면체 한번 더 굴리기/cube-rounding-again.py
@@ -1,8 +1,6 @@
 import collections
 import sys
 
-# sys.stdin = open('정육면체 한번 더 굴리기', 'r')
-
 
 def turn_right(up, bottom, right, left, front, rear):
     up, right, bottom, left = left, up, right, bottom
@@ -23,30 +21,33 @@ def turn_front(up, bottom, right, left, front, rear):
 
 
 def turn_rear(up, bottom, right, left, front, rear):
-    up, front, bottom, rear = rear, up, front, bottom
+    up, front, bottom, rear = front, bottom, rear, up
 
     return up, bottom, right, left, front, rear
 
 
-def get_score(x, y):
-    q = collections.deque()
-    q.append((x, y))
-    ret = board[x][y]
-    visited = [[False] * N for _ in range(N)]
-    visited[x][y] = True
+def DP():
+    def bfs(x, y):
+        q = collections.deque()
+        q.append((x, y))
+        total = board[x][y]
+        path = {(x, y)}
+        while q:
+            x, y = q.popleft()
+            for dx, dy in dir:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < N and 0 <= ny < N and (nx, ny) not in path and board[nx][ny] == board[x][y]:
+                    path.add((nx, ny))
+                    q.append((nx, ny))
+                    total += board[nx][ny]
 
-    while q:
-        x, y = q.popleft()
+        for px, py in path:
+            dp[px][py] = total
 
-        for dx, dy in dir:
-            nx, ny = x + dx, y + dy
-
-            if 0 <= nx < N and 0 <= ny < N and board[x][y] == board[nx][ny] and not visited[nx][ny]:
-                visited[nx][ny] = True
-                ret += board[nx][ny]
-                q.append((nx, ny))
-
-    return ret
+    for i in range(N):
+        for j in range(N):
+            if dp[i][j] == 0:
+                bfs(i, j)
 
 
 if __name__ == '__main__':
@@ -60,36 +61,51 @@ if __name__ == '__main__':
     N, M = map(int, input().split())
 
     board = [list(map(int, input().split())) for _ in range(N)]
+    dp = [[0] * N for _ in range(N)]
+    DP()
     # 방향
     d = 0
     # 현재 좌표
-    x, y = 0, 1
+    x, y = 0, 0
     result = 0
 
-    # 오른쪽으로 한칸 움직이기
-    up, bottom, right, left, front, rear = turn_right(up, bottom, right, left, front, rear)
-    result += get_score(x, y)
-
-    for _ in range(M - 1):
-        if bottom > board[x][y]:
-            d = (d + 1) % 4
-        elif bottom < board[x][y]:
-            d = (d - 1) % 4
-        dx, dy = dir[d]
-        nx, ny = x + dx, y + dy
-        if not (0 <= nx < N and 0 <= ny < N):
-            d = (d + 2) % 4
+    for i in range(M):
+        # 오른쪽으로
+        if i == 0:
+            up, bottom, right, left, front, rear = turn_right(up, bottom, right, left, front, rear)
+            # 좌표만 움직이기
             dx, dy = dir[d]
             nx, ny = x + dx, y + dy
-        x, y = nx, ny
-        result += get_score(x, y)
-        if d == 0:
-            up, bottom, right, left, front, rear = turn_right(up, bottom, right, left, front, rear)
-        elif d == 1:
-            up, bottom, right, left, front, rear = turn_rear(up, bottom, right, left, front, rear)
-        elif d == 2:
-            up, bottom, right, left, front, rear = turn_left(up, bottom, right, left, front, rear)
-        elif d == 3:
-            up, bottom, right, left, front, rear = turn_front(up, bottom, right, left, front, rear)
+            if not (0 <= nx < N and 0 <= ny < N):
+                d = (d + 2) % 4
+                dx, dy = dir[d]
+                nx, ny = x + dx, y + dy
+            x, y = nx, ny
+            result += dp[x][y]
+        else:
+            if bottom > board[x][y]:
+                d = (d + 1) % 4
+            elif bottom < board[x][y]:
+                d = (d - 1) % 4
+            # 좌표만 움직이기
+            dx, dy = dir[d]
+            nx, ny = x + dx, y + dy
+            if not (0 <= nx < N and 0 <= ny < N):
+                d = (d + 2) % 4
+                dx, dy = dir[d]
+                nx, ny = x + dx, y + dy
+            x, y = nx, ny
+            result += dp[x][y]
+            # 회전
+            if d == 0:
+                up, bottom, right, left, front, rear = turn_right(up, bottom, right, left, front, rear)
+            elif d == 1:
+                up, bottom, right, left, front, rear = turn_front(up, bottom, right, left, front, rear)
+            elif d == 2:
+                up, bottom, right, left, front, rear = turn_left(up, bottom, right, left, front, rear)
+            elif d == 3:
+                up, bottom, right, left, front, rear = turn_rear(up, bottom, right, left, front, rear)
+
+
 
     print(result)
