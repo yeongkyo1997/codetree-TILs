4ceac011e833e6d31d6fdaddf0874f01304f1bac
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-04 09:49:52 +0900

    Title: 블럭의 총 둘레, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241004/블럭의 총 둘레/total-perimeter-of-blocks.py
@@ -0,0 +1,40 @@
+import collections
+import sys
+
+sys.stdin = open('블록의 총 둘레', 'r')
+
+
+def bfs(x, y):
+    q = collections.deque()
+    visited = set()
+    visited.add((x, y))
+    q.append((x, y))
+
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in dir:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < N and 0 <= ny < N and (nx, ny) not in visited:
+                if board[nx][ny] == 1:
+                    cnt_board[nx][ny] += 1
+                else:
+                    q.append((nx, ny))
+                    visited.add((nx, ny))
+
+
+if __name__ == '__main__':
+    dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    block_num = int(input())
+
+    N = 110
+
+    board = [[0] * N for _ in range(N)]
+    cnt_board = [[0] * N for _ in range(N)]
+    for _ in range(block_num):
+        r, c = map(int, input().split())
+        
+        board[r][c] = 1
+    bfs(0, 0)
+    print(sum(sum(b) for b in cnt_board))
